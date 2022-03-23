a35d60c34e09ebf292ece42c7fa4325fda6d6992
ayushvarma7 <ayushvarma04@gmail.com>
2022-03-23 19:26:56 +0530

    Added List feature

---
+++ src/components/List/test.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int a, b;
+
+    cout<<"Enter a number: ";
+    cin>>a;
+
+cout<<"The multiples of "<<a <<" are: ";
+    for(int i=0; i<10; i++)
+        cout<<a*i<<endl;
+    return 0;
+}
